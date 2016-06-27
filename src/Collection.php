b5a5836e9dbcc8c08387b9ddcab9f2965783d008
Hixman <hixman88@gmail.com>
2016-06-28 00:39:38 +0200

    Clean up

--- src/Collection.php
+++ src/Collection.php
@@ -21,23 +21,6 @@ class Collection implements Iterator
     }
 
     /**
-     * Add a item to dataset attached to a key
-     *
-     * @param  mixed  $key
-     * @param  mixed  $value
-     * @return Collection
-     */
-    public function addWithKey($key, $value)
-    {
-        if (isset($this->items[$key])) {
-            $this->items[$key]->add($value);
-        } else {
-            $this->items[$key] = new Collection([$value]);
-        }
-        return $this;
-    }
-
-    /**
      * Apply map function to items
      *
      * @param  function  $function
@@ -58,70 +41,6 @@ class Collection implements Iterator
     public function reduce(callable $function, $initial = null)
     {
         return array_reduce($this->items, $function, $initial);
-    }
-
-    /**
-     * Group by given function
-     *
-     * @param  function  $function
-     * @return mixed
-     */
-    public function groupBy(callable $function = null)
-    {
-
-        return $this->reduce(function ($result, $item) use ($function) {
-            return $result->addWithKey($function($item), $item);
-        }, new Collection([]));
-    }
-
-
-    /**
-     * Sort by given function
-     *
-     * @param  function  $function
-     * @return mixed
-     */
-    public function sortBy(callable $function, $descending = false)
-    {
-        $sortedItems = array_map(function ($item) use ($function) {
-            return $function($item);
-        }, $this->items);
-        ($descending)?arsort($sortedItems):asort($sortedItems);
-        $keys = array_keys($sortedItems);
-        $this->items = array_map(function ($key) {
-            return $this->items[$key];
-        }, $keys);
-        return $this;
-    }
-
-    /**
-     * create an array with n item taken from the beginning
-     *
-     * @param  function  $numberOfElements
-     * @param  bool  $perserveKeys
-     * @return Collection
-     */
-    public function take($numberOfElements, $perserveKeys = true)
-    {
-        $sliceTaken = array_slice($this->items, 0, $numberOfElements, $perserveKeys);
-        $this->items = array_slice($this->items, $numberOfElements);
-        return new Collection($sliceTaken);
-    }
-
-    /**
-     * create an array with n item filtered by given function
-     *
-     * @param  function  $function
-     * @return mixed
-     */
-    public function filter(callable $function)
-    {
-        return $this->reduce(function ($collection, $row) use ($function) {
-            if ($function($row)) {
-                return $collection->add($row);
-            }
-            return $collection;
-        }, new Collection());
     }
 
     /**
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -11,9 +11,6 @@ use League\Functional\Collectibles\Arr;
 
 class CollectionTest extends PHPUnit_Framework_TestCase
 {
-    const ODD = "odd";
-    const EVEN = "even";
-    const SORTER = "sorter";
 
     /**
      * @var Collection
@@ -43,8 +40,8 @@ class CollectionTest extends PHPUnit_Framework_TestCase
      */
     public function test_map_function()
     {
-        $expectedArray = [2,3,4,5];
-        $this->collection =  new Collection([1,2,3,4]);
+        $this->collection =  new Collection([1,2,3,4,5]);
+        $expectedArray = new Collection([2,3,4,5,6]);
         $function = function($value)
         {
             return $value+1;
@@ -53,8 +50,7 @@ class CollectionTest extends PHPUnit_Framework_TestCase
         $this->collection->map($function);
 
         $collection = $this->collection;
-        $testOk = $this->iterateAndMatchWithExpectedArray($collection, $expectedArray);
-        $this->assertTrue($testOk);
+        $this->assertTrue($this->compareTwoCollections($collection, $expectedArray));
     }
 
     /**
@@ -66,7 +62,7 @@ class CollectionTest extends PHPUnit_Framework_TestCase
         $expectedResultWithInitialStatus = 5;
         $initialStatus = 1;
         $this->collection =  new Collection([1,1,1,1]);
-                $function = function($result,$value)
+        $function = function($result,$value)
         {
             return $result + $value;
         };
@@ -76,76 +72,6 @@ class CollectionTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue($result == $expectedResult);
         $this->assertTrue($resultWithInitialStatus == $expectedResultWithInitialStatus);
-    }
-
-    /**
-     * Test groupBy function works properly
-     */
-    public function test_group_by_function()
-    {
-        $this->collection = new Collection([1,2,3,4,5,6,7,8,9,10]);
-        $function = function($item)
-        {
-            return ($item%2 == 0)?self::EVEN:self::ODD;
-        };
-
-        $groupedByOddEven = $this->collection->groupBy($function);
-
-        $testOK = $this->iterateOddAndEvenAndCheckIfDataIsOddOrEven($groupedByOddEven);
-        $this->assertTrue($testOK);
-    }
-
-
-    /**
-     * Test groupBy resultset are collections
-     */
-    public function test_group_by_resultset_are_collections()
-    {
-        $this->collection = new Collection([1,2,3,4,5,6,7,8,9,10]);
-        $function = function($item)
-        {
-            return ($item%2 == 0)?self::EVEN:self::ODD;
-        };
-
-        $groupedByOddEven = $this->collection->groupBy($function);
-
-        $testOK = true;
-        foreach ($groupedByOddEven as $collection)
-        {
-            $testOK = $collection instanceof Collection;
-        }
-        $this->assertTrue($testOK);
-    }
-
-    /**
-     * Test groupBy function works properly
-     */
-    public function test_sort_by_function()
-    {
-        $sorters =[
-            [self::SORTER =>1],
-            [self::SORTER =>5],
-            [self::SORTER =>3],
-            [self::SORTER =>2],
-            [self::SORTER =>4]
-        ];
-        $expectedArray = [
-            [self::SORTER =>1],
-            [self::SORTER =>2],
-            [self::SORTER =>3],
-            [self::SORTER =>4],
-            [self::SORTER =>5]
-        ];
-        $this->collection = new Collection($sorters);
-        $function = function($item)
-        {
-            return $item[self::SORTER];
-        };
-
-        $sortedCollection = $this->collection->sortBy($function);
-
-        $testOK = $this->iterateAndCheckIfResultMatchesWithExpected($sortedCollection, $expectedArray);
-        $this->assertTrue($testOK);
     }
 
     /**
@@ -177,7 +103,7 @@ class CollectionTest extends PHPUnit_Framework_TestCase
      */
     public function test_iterators_method()
     {
-        $this->collection = new Collection([1,2,3,4]);
+        $this->collection = new Collection([1,2,3,4,5]);
 
         $keyTest = $this->collection->key() == 0;
         $this->collection->next();
@@ -204,19 +130,16 @@ class CollectionTest extends PHPUnit_Framework_TestCase
     {
         $this->collection = new Collection([1,2,3,4,5]);
         $this->collection2 = new Collection([1,2,3,4,5]);
+        $expectedCollection = new Collection([1,2,3,4]);
         $expectedResult = 5;
-        $expectedArray = [1,2,3,4];
 
         $result = $this->collection->last();
         $this->assertTrue($result == $expectedResult);
         $result = $this->collection2->last(true);
         $this->assertTrue($result == $expectedResult);
 
-        $testAllItemsAreEquals = true;
-        foreach ($this->collection2 as $index => $item) {
-            $testAllItemsAreEquals = $item == $expectedArray[$index];
-        }
-        $this->assertTrue($testAllItemsAreEquals);
+        $this->assertTrue($this->compareTwoCollections($expectedCollection,$this->collection2));
+
     }
 
     /**
@@ -226,8 +149,8 @@ class CollectionTest extends PHPUnit_Framework_TestCase
     {
         $this->collection = new Collection([1,2,3,4,5]);
         $this->collection2 = new Collection([1,2,3,4,5]);
+        $expectedCollection = new Collection([2,3,4,5]);
         $expectedResult = 1;
-        $expectedArray = [2,3,4,5];
 
         $resultWithoutRemove = $this->collection->first();
         $this->assertTrue($resultWithoutRemove == $expectedResult);
@@ -235,43 +158,7 @@ class CollectionTest extends PHPUnit_Framework_TestCase
         $resultWithRemove = $this->collection2->first(true);
         $this->assertTrue($resultWithRemove == $expectedResult);
 
-        $testAllItemsAreEquals = true;
-        foreach ($this->collection2 as $index => $item) {
-            $testAllItemsAreEquals = $item == $expectedArray[$index];
-        }
-        $this->assertTrue($testAllItemsAreEquals);
-    }
-
-    /**
-     * Test lastItem method
-     */
-    public function test_take_method()
-    {
-        $this->collection = new Collection([1,2,3,4,5]);
-        $expectedCollection = new Collection([1,2]);
-        $remainingCollection = new Collection([3,4,5]);
-
-        $resultTakingTwoItems = $this->collection->take(2);
-
-        $this->assertTrue($this->compareTwoCollections($expectedCollection,$resultTakingTwoItems));
-        $this->assertTrue($this->compareTwoCollections($remainingCollection,$this->collection));
-    }
-
-    /**
-     * Test filter method
-     */
-    public function test_filter_method()
-    {
-        $this->collection = new Collection([1,2,3,4,5,6,7,8,9,10]);
-        $expectedCollection = new Collection([2,4,6,8,10]);
-        $function = function ($item)
-        {
-            return $item % 2 == 0;
-        };
-
-        $resultTakingTwoItems = $this->collection->filter($function);
-
-        $this->assertTrue($this->compareTwoCollections($expectedCollection,$resultTakingTwoItems));
+        $this->assertTrue($this->compareTwoCollections($expectedCollection,$this->collection2));
     }
 
     /**
@@ -282,52 +169,6 @@ class CollectionTest extends PHPUnit_Framework_TestCase
         $this->setExpectedException(InvalidArgumentException::class);
         $this->collection = new Collection(3);
         $this->collection = new Collection("A string");
-    }
-
-    /**
-     * @param $collection
-     * @param $expectedArray
-     * @return bool
-     */
-    private function iterateAndMatchWithExpectedArray($collection, $expectedArray)
-    {
-        $testOk = true;
-        $array = $collection->all();
-        for ($i = 0; $i < count($expectedArray); $i++) {
-            if ($array[$i] != $expectedArray[$i]) $testOk = false;
-        }
-        return $testOk;
-    }
-
-    /**
-     * @param $groupedByOddEven
-     * @return bool
-     */
-    private function iterateOddAndEvenAndCheckIfDataIsOddOrEven($groupedByOddEven)
-    {
-        $testOK = true;
-        foreach ($groupedByOddEven->getByKey(self::ODD) as $value) {
-            if ($value % 2 == 0) $testOK = false;
-        }
-        foreach ($groupedByOddEven->getByKey(self::EVEN) as $value) {
-            if ($value % 2 != 0) $testOK = false;
-        }
-        return $testOK;
-    }
-
-    /**
-     * @param $sortedCollection
-     * @param $expectedArray
-     * @return bool
-     */
-    private function iterateAndCheckIfResultMatchesWithExpected($sortedCollection, $expectedArray)
-    {
-        $testOK = true;
-        $sortedArray = $sortedCollection->all();
-        for ($i = 0; $i < count($expectedArray); $i++) {
-            if ($expectedArray[$i][self::SORTER] != $sortedArray[$i][self::SORTER]) $testOK = false;
-        }
-        return $testOK;
     }
 
     /**
